81d701fff4d3aeb10fa055a7794790b0711887b9
eitan Fixe <allaleitan@gmail.com>
2026-01-20 16:53:40 +0200

    fix: cleaned code

--- main.py
+++ main.py
@@ -19,25 +19,30 @@ def get_ip_country(ip):
         response = reader.city(ip)
         country = response.country.name if response.country.name else "Unknown"
         return country
-    except:
+    except Exception as e:
         return "Not found"  # If the IP address isn't found in the database
 
-def get_file(file_path = './apache_log.txt'):
+
+def read_file(file_path='./apache_log.txt'):
     try:
         with open(file_path, 'r', encoding='utf-8') as f:
-            content = f.read().split('\n')[:-1]
+            return f.read().strip().split('\n')
     except FileNotFoundError:
         print(f"Error: The file '{file_path}' was not found.")
     except Exception as e:
         print(f"An error occurred: {e}")
-    return content
+    return []
 
-def parse_file(content):
-    ip_list = []
-    os_list=[]
-    browser_list=[]
+def is_bot(user_agent):
+    return any(bot.lower() in user_agent.lower() for bot in BOT_KEYWORDS)
+
+def extract_data(content):
+    ip_list, os_list, browser_list= [], [], []
+    ip_pattern = r'\d+\.\d+\.\d+\.\d+'
+    os_pattern = r'(Windows|Mac OS X|Linux|Android|Darwin|iPhone|FreeBSD|OpenBSD|SunOS|Windows Phone)'
+    browser_pattern = r'(MSIE|Chrome|Trident|Firefox|BonEcho|Safari|Opera|Edge)'
+
     for line in content:
-        ip_pattern = r'\d+\.\d+\.\d+\.\d+'
         ip = re.search(ip_pattern, line)
         if ip is not None:
             ip_list.append(get_ip_country(ip.group()))
@@ -51,33 +56,24 @@ def parse_file(content):
             browser_list.append("bot")
 
         else:
-            os_pattern = r'(Windows|Mac OS X|Linux|Android|Darwin|iPhone|FreeBSD|OpenBSD|SunOS|Windows Phone)'
             os=re.search(os_pattern, user_agent)
-
-            browser_pattern = r'(MSIE|Chrome|Trident|Firefox|BonEcho|Safari|Opera|Edge)'
             browser = re.search(browser_pattern, user_agent)
 
-            if os is not None:
-                os_list.append(os.group())
-            else:
-                os_list.append("Unknown")
+            os_list.append(os.group() if os else "Unknown")
+            browser_list.append(browser.group() if browser else "Unknown")
+    return ip_list, os_list, browser_list
 
-            if browser is not None:
-                browser_list.append(browser.group())
-            else:
-                browser_list.append("Unknown")
 
-    df = pd.DataFrame({
+def create_dataframe(ip_list, os_list, browser_list):
+    return pd.DataFrame({
         'IP country': ip_list,
         'OS': os_list,
         'Browser': browser_list
     })
-    print(f"Number of identified bots: {os_list.count('bot')}")
-    return df
 
-def create_stats(df):
-    total_hits = len(df)
-    country_hits = df.groupby('IP country').size().sort_values(ascending=False)
+def create_stats(dataframe):
+    total_hits = len(dataframe)
+    country_hits = dataframe.groupby('IP country').size().sort_values(ascending=False)
     country_percentage = country_hits/total_hits*100
 
     filtered_countries_percentage = country_percentage[country_percentage>1]
@@ -85,34 +81,29 @@ def create_stats(df):
     if other_countries > 0:
         filtered_countries_percentage['Other']= other_countries
 
-    os_hits = df.groupby('OS').size().sort_values(ascending=False)
-    os_percentage = os_hits/total_hits*100
-
-    browser_hits = df.groupby('Browser').size().sort_values(ascending=False)
-    browser_percentage = browser_hits/total_hits*100
+    os_percentage = dataframe.groupby('OS').size().sort_values(ascending=False)/total_hits*100
+    browser_percentage = dataframe.groupby('Browser').size().sort_values(ascending=False)/total_hits*100
     return filtered_countries_percentage, os_percentage, browser_percentage
 
-def write_output(country_stats, os_stats, browser_stats):
-    with open('output/log_report.txt', 'w') as f:
-        f.write('### Country Statistics ###\n')
+def write_output(country_stats, os_stats, browser_stats, output_path='output/log_report.txt'):
+    """Write the statistics to an output file."""
+    with open(output_path, 'w') as f:
+        f.write('Country:\n')
         for country, percentage in country_stats.items():
-            f.write(f"{country}: {percentage: .2f}%\n")
-        f.write("\n")
-
-        f.write('### OS Statistics ###\n')
+            f.write(f"{country}: {percentage:.2f}%\n")
+        f.write("\nOS\n")
         for os, percentage in os_stats.items():
-            f.write(f"{os}: {percentage: .2f}%\n")
-        f.write("\n")
-
-        f.write('### Browser Statistics ###\n')
+            f.write(f"{os}: {percentage:.2f}%\n")
+        f.write("\nBrowser\n")
         for browser, percentage in browser_stats.items():
-            f.write(f"{browser}: {percentage: .2f}%\n")
-        f.write("\n")
+            f.write(f"{browser}: {percentage:.2f}%\n")
 
-    print("Log saved to output/log_report.txt")
+    print(f"Log saved to {output_path}")
 
 if __name__ == "__main__":
-    file_content = get_file()
-    parsed = parse_file(file_content)
-    countries, OSs, browsers = create_stats(parsed)
-    write_output(countries, OSs, browsers)
+    file_content = read_file()
+    if file_content:
+        ips, OSs, browsers = extract_data(file_content)
+        df = create_dataframe(ips, OSs, browsers)
+        country_statistics, os_statistics, browser_statistics = create_stats(df)
+        write_output(country_statistics, os_statistics, browser_statistics)
